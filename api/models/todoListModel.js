fcca23a21d2f08cecf04851f8a17d179968c5c21
Yatender Poonia <yatenderpoonia.yp@gmail.com>
2017-06-27 16:57:02 +0530

    message

---
+++ api/models/todoListModel.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+var mongoose = require('mongoose');
+var TaskSchema = mongoose.Schema
+({
+  name: {
+    type: String,
+    Required: 'Kindly enter the name of the task'
+  },
+    email:{
+    type:String,
+        Required: 'Kindly enter the email of the task'
+    },
+    mobile:{
+    type:String
+    },
+    message:{
+    type:String
+    },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+
+module.exports = mongoose.model('Tasks', TaskSchema);
