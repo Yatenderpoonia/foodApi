dccaff997183eb7c3c49244d0b442f18037cd984
Yatender Poonia <yatenderpoonia.yp@gmail.com>
2017-06-28 11:46:43 +0530

    fix bug

--- server.js
+++ server.js
@@ -8,7 +8,20 @@ var express = require('express'),
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://yatenderpoonia1:yatender123@ds141242.mlab.com:41242/food_database');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+app.use(allowCrossDomain)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -23,5 +36,6 @@ app.use(function(req, res) {
 app.listen(port,function(){
   console.log('app listging on port %d',process.env.PORT)
 });
+
 
 
