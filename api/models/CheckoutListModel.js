e071c74c1ad188b399ad752244c6ff86e07dd23b
Yatender Poonia <yatenderpoonia.yp@gmail.com>
2017-06-29 10:13:49 +0530

    bug fix

--- api/models/CheckoutListModel.js
+++ api/models/CheckoutListModel.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 
 var checkoutSchema=mongoose.Schema({
     name:{type:String},
-    address:{type:String},
+    Delivery_address:{type:String},
     payment:{"type":String,"number":Number,"expire":String,"cvc":Number},
     items:[{"name":String,"price":String,"qty":Number}],
     total:{type:Number}
