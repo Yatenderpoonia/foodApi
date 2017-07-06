8178e35e4a2053c912086cf0a625bedac81bd2fe
Yatender Poonia <yatenderpoonia.yp@gmail.com>
2017-07-06 16:47:44 +0530

    resturant search

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var express = require('express'),
   Task = require('./api/models/todoListModel'),
   bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://yatenderpoonia1:yatender123@ds141242.mlab.com:41242/food_database');
+//mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://yatenderpoonia1:yatender123@ds141242.mlab.com:41242/food_database');
 mongoose.connect('mongodb://localhost/Tododb');
 
 var allowCrossDomain = function(req, res, next) {
